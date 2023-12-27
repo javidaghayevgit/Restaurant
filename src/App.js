d6de7f9c569c8e1cbd9ffeaaa9f9c9108cc6df14
JavidAghayev <contactwithjavid@gmail.com>
2023-12-27 17:54:02 +0400

    change in router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Header from './components/Header'
+import Home from './pages/Home/index'
+import About from './pages/About/index'
+import Contact from './pages/Contact/index'
+import Header from './components/Header/index'
 import Menu from './pages/Menu/index'
-import Blog from './pages/Blog';
-import Reservation from './pages/Reservation';
+import Blog from './pages/Blog/index'
+import Reservation from './pages/Reservation/index';
 function App() {
   return (
     <div className="app">
--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -2,9 +2,6 @@ import '../Blog/style.css'
 const Blog=()=>{
     return(
         <div className=" blog">
-            <div className="slider_item">
-                    <div className="overlay"></div>
-                </div>
         </div>
     )
 }
